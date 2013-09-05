234c71391b512f434c16dd172601b7c166a7cd4a
Jonas Baumann <jone@jone.ch>
2013-09-05 14:45:52 +0200

    peter

--- ftw/solr/patches/reindex.py
+++ ftw/solr/patches/reindex.py
@@ -1,16 +1,25 @@
 from Acquisition import aq_base
-from collective.indexing.queue import getQueue
-from collective.indexing.queue import processQueue
-from logging import WARNING
 from Products.Archetypes.config import TOOL_NAME
 from Products.Archetypes.log import log
 from Products.Archetypes.utils import isFactoryContained
 from Products.CMFCore.interfaces import ICatalogTool
 from Products.CMFCore.utils import getToolByName
+from collective.indexing.queue import getQueue
+from collective.indexing.queue import processQueue
+from logging import WARNING
+from plone.indexer.interfaces import IIndexer
+from zope.component import queryMultiAdapter
 import logging
 
 
 logger = logging.getLogger('ftw.solr')
+
+def is_index_value_equal(old, new):
+    if type(old) != type(new):
+        return False
+    if isinstance(old, list):
+        return set(old) == set(new)
+    return old == new
 
 
 def ftw_solr_CatalogMultiplex_reindexObjectSecurity(self, skip_self=False):
@@ -28,6 +37,7 @@ def ftw_solr_CatalogMultiplex_reindexObjectSecurity(self, skip_self=False):
     # Account for name mangling of double underscore attributes
     path = self._CatalogMultiplex__url()
 
+    skipped = 0
     for catalog in catalogs:
         for brain in catalog.unrestrictedSearchResults(path=path):
             brain_path = brain.getPath()
@@ -48,9 +58,31 @@ def ftw_solr_CatalogMultiplex_reindexObjectSecurity(self, skip_self=False):
                     brain_path, level=WARNING)
                 continue
 
-            # Also update relevant security indexes in solr
-            indexer = getQueue()
-            indexer.reindex(ob, self._cmf_security_indexes)
+            indexed_values = catalog._catalog.getIndexDataForRID(brain.getRID())
+            changed_indexes = []
+            for index_name in self._cmf_security_indexes:
+                indexer = queryMultiAdapter((ob, catalog), IIndexer,
+                                            name=index_name)
+                if indexer is None:
+                    indexer = getattr(ob, index_name, None)
+
+                if indexer is None:
+                    changed_indexes.append(index_name)
+                else:
+                    new_value = indexer()
+                    if not is_index_value_equal(indexed_values[index_name],
+                                                new_value):
+                        changed_indexes.append(index_name)
+
+            if len(changed_indexes) > 0:
+                print 'INDEXING', self, ob.absolute_url()
+                print '        BEFORE:', indexed_values[index_name]
+                print '         AFTER:', new_value
+                indexer = getQueue()
+                indexer.reindex(ob, changed_indexes)
+            else:
+                skipped += 1
+    print 'SKIP', skipped
 
 
 def ftw_solr_CatalogAware_reindexObjectSecurity(self, skip_self=False):
